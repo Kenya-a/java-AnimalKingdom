cd77c68002ac9bb89bba578b23bee5dc4a6fbd25
Kenya-a <Kenya-a@users.noreply.github.com>
2019-09-04 18:25:30 -0500

    sorting in descending order

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -24,34 +24,61 @@ public class Main
         //Mammals
         myList.add(new Mammals("Panda", 1869));
         myList.add(new Mammals("Zebra", 1778));
-        myList.add(new Mammals("Koala", 1778));
-        myList.add(new Mammals("Sloth", 1778));
-        myList.add(new Mammals("Armadillo", 1778));
-        myList.add(new Mammals("Raccoon", 1778));
-        myList.add(new Mammals("BigFood", 1778));
+        myList.add(new Mammals("Koala", 1816));
+        myList.add(new Mammals("Sloth", 1804));
+        myList.add(new Mammals("Armadillo", 1758));
+        myList.add(new Mammals("Raccoon", 1758));
+        myList.add(new Mammals("BigFood", 2021));
 
         //Fish
-        myList.add(new Fish("Salmon", 1778));
-        myList.add(new Fish("Catfish", 1778));
-        myList.add(new Fish("Perch", 1778));
+        myList.add(new Fish("Salmon", 1758));
+        myList.add(new Fish("Catfish", 1817));
+        myList.add(new Fish("Perch", 1758));
 
         //Birds
-
-        myList.add(new Birds("Pigeon", 1778));
-        myList.add(new Birds("Peacock", 1778));
-        myList.add(new Birds("Toucan", 1778));
-        myList.add(new Birds("Parrot", 1778));
-        myList.add(new Birds("Swan", 1778));
+        myList.add(new Birds("Pigeon", 1837));
+        myList.add(new Birds("Peacock", 1821));
+        myList.add(new Birds("Toucan", 1758));
+        myList.add(new Birds("Parrot", 1824));
+        myList.add(new Birds("Swan", 1758));
         
         
 
+        //1.List all the animals in descending order by year named
+        System.out.println("*** Sorting in Descending Order of Discovery Year ***");
+        myList.sort((a1, a2) -> a1.getDiscovery() - a2.getDiscovery());
+        myList.forEach(animal -> System.out.println(animal.getName() + ":" + animal.getDiscovery()));
 
-        //1. List all the animals alphabetically.
+        System.out.println();
+
+
+
+        //2. List all the animals alphabetically.
         System.out.println("*** Sorting in Alphabetical Order ***");
         myList.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
         myList.forEach(animal -> System.out.println(animal.getName()));
-      
+        System.out.println();
 
+
+        //3.List all the animals order by how they move
+        System.out.println();
+        System.out.println();
+
+        //4.List only those animals the breath with lungs
+        System.out.println();
+        System.out.println();
+
+        //5.List only those animals that breath with lungs and were named in 1758
+        System.out.println();
+        System.out.println();
+
+        //6.List only those animals that lay eggs and breath with lung
+        System.out.println();
+        System.out.println();
+
+        //7.List alphabetically only those animals that were named in 1758
+        System.out.println();
+        System.out.println();
 
 
     }
